d310106281ac48b77d584e5a6c3b8eef45efa267
zetavg <mail@zeta.vg>
2018-09-10 22:57:03 +0800

    update

--- scripts/_patch_extensions.rb
+++ scripts/_patch_extensions.rb
@@ -8,12 +8,17 @@ def patch_extensions(extensions_dir, icon_set: 'default')
   mappings = YAML.load_file(File.join(icon_set_dir, 'mappings.yml'))
   list = extensions_list(extensions_dir)
 
+  puts "\n  Installed extensions:"
+  list.each do |_extension_id, extension_data|
+    puts "  - #{extension_data[:name]} at #{extension_data[:directory]}"
+  end
+
   list.each do |_extension_id, extension_data|
     extension_name = extension_data[:name]
     extension_dir = extension_data[:directory]
 
     next unless mappings.has_key?(extension_name)
-    puts "== Patching #{extension_name}"
+    puts "\n== Patching #{extension_name}"
 
     mappings[extension_name] && mappings[extension_name].each do |original_file, replacement_file|
       backup_and_replace(File.join(extension_dir, original_file), File.join(icon_set_dir, replacement_file))
